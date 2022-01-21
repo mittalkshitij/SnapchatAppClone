986d295265a0fa994e9307d335ae1d4df2d4192f
Kshitij <mittalkshitij20@gmail.com>
2022-01-21 19:43:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/snapchatclone/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.snapchatclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    var emailEditText:EditText?=null
+    var passwordEditText:EditText?=null
+    val mAuth=FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        emailEditText=findViewById(R.id.emailEditText)
+        passwordEditText=findViewById(R.id.passwordEditText)
+
+        if(mAuth.currentUser!=null)
+        {
+            login()
+        }
+    }
+
+    fun loginClicked(view:View)
+    {
+        mAuth.signInWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Login Successfull
+                    login()
+                } else {
+                    //Login Failed.. Sign up user
+                    mAuth.createUserWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful)
+                        {
+                            login()
+                        }
+                        else
+                        {
+                            Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+    }
+
+    fun login()
+    {
+        val intent= Intent(this,SnapsActivity::class.java)
+        startActivity(intent)
+    }
+}
