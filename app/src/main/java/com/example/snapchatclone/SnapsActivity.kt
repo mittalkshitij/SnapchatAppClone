5ec4856c4e14f00f2bdc6bb661d8feccf6de6228
Kshitij <mittalkshitij20@gmail.com>
2022-01-22 20:40:00 +0530

    Committing after displaying users

--- app/src/main/java/com/example/snapchatclone/SnapsActivity.kt
+++ app/src/main/java/com/example/snapchatclone/SnapsActivity.kt
@@ -5,15 +5,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.snapchatclone.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class SnapsActivity : AppCompatActivity() {
 
     val mAuth= FirebaseAuth.getInstance()
+
+    var snapsListView:ListView?=null
+    var emails: ArrayList<String> = ArrayList()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snaps)
+
+        snapsListView=findViewById(R.id.snapsListView)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, emails)
+        snapsListView?.adapter = adapter
+
+        mAuth.currentUser?.uid?.let {
+            FirebaseDatabase.getInstance().reference.child("users").child(
+                it
+            ).child("snaps").addChildEventListener(object : ChildEventListener{
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                        emails.add(snapshot.child("from").value as String)
+                        adapter.notifyDataSetChanged()
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+                override fun onChildRemoved(snapshot: DataSnapshot) {}
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+                override fun onCancelled(error: DatabaseError) {}
+            })
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
